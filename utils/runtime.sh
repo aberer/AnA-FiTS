c4a16bb640dc9d635d52d453fef23bb9b8bd7aca
Andre J. Aberer <andre.aberer@h-its.org>
2012-08-14 16:26:36 +0200

    update runtime

--- utils/runtime.sh
+++ utils/runtime.sh
@@ -6,12 +6,19 @@ if [ $# != 2 ]; then
 fi
 
 neutral=$1
+
+if [ "$neutral" == "0" ]; then
+    fitness="-w"
+else     
+    fitness="-W 2 $neutral 10 2000000 $neutral 1 5000000"
+fi
+
 id=$2
 
 Ne="100 1000 10000"
 L="10000 100000 1000000 10000000"
 param="2.5e-8" 
-fitness="2 $neutral 10 2000000 $neutral 1 5000000"
+
 
 binary=$(ls ./AnA-FiTS-* | tail -n 1 ) 
 
@@ -20,7 +27,7 @@ for N in $(echo $Ne)
 do     
     for l in $(echo $L)
     do 
-	$binary -W $fitness -s $id -N $N -L $l -m $param -r $param -n run.$N.$l.$neutral.$id & 
+	$binary  $fitness -s $id -N $N -L $l -m $param -r $param -n run.$N.$l.$neutral.$id & 
     done 
 done 
 
