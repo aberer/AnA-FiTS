f69e5ba31f04f943af4f1802642b5577c8c1cf9f
Andre Aberer <andre.aberer@h-its.org>
2012-10-23 12:03:40 +0200

    bug in tuning script

--- utils/tuneCoalParam.sh
+++ utils/tuneCoalParam.sh
@@ -8,11 +8,13 @@ L="10000 50000 100000 500000 1000000"
 param="2.5e-8" 
 
 for tuneParam in $(seq 2 10  )
-for N in $(echo $Ne) 
-do     
-    for l in $(echo $L)
-    do 
-	$binary -w -R $tuneParam  -s 1  -N $N -L $l -m $param -r $param -n run.$N.$l.$id >  run.$N.$l.$tuneParam  &  
-	sleep 1 
-    done 
-done
+do 
+    for N in $(echo $Ne) 
+    do     
+	for l in $(echo $L)
+	do 
+	    $binary -w -R $tuneParam  -s 1  -N $N -L $l -m $param -r $param -n run.$N.$l.$id >  run.$N.$l.$tuneParam  &  
+	    sleep 1 
+	done 
+    done
+done 
