a8a088a769875ed626e104ee2c396bfc7559ce6d
Andre Aberer <andre.aberer@h-its.org>
2012-11-02 17:41:51 +0100

    automated pre-tag commit

--- utils/tag.sh
+++ utils/tag.sh
@@ -5,13 +5,13 @@ if [ "$#" != "1"  ]; then
     exit 
 fi				# 
 
-git tag -a "$1" -m "stable release version $1"
+git tag -a $1 -m "stable release version $1"
 make clean
 make vupdate 
 
-git tag -d "$1 "
+git tag -d $1 
 git commit -a -m "automated pre-tag commit"
-git tag -a "$1" -m "release version $1" 
+git tag -a $1 -m "release version $1" 
 git push
 git push --tags
 
